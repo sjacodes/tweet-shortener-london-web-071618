72beca6182a910f94bb707df9b7a9cd703735659
Sarah Jacob <smjacob91@gmail.com>
2018-07-08 07:39:41 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -30,13 +30,3 @@ def word_substituter(tweet)
   return individual_words.join(" ")
 end
 
-
-
-      
-  
-  # Question 1
-  describe '#word_substituter' do
-    it 'replaces long words with their expected short form' do
-      expect(word_substituter(tweet_one)).to eq(tweet_one_short)
-    end
-  end
