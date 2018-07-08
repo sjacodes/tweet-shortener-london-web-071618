ec9af5121dffd7a8a66323019682b77d7f8f4beb
Sarah Jacob <smjacob91@gmail.com>
2018-07-08 08:01:18 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -39,7 +39,7 @@ def bulk_tweet_shortener(tweets)
 end
   
 def selective_tweet_shortener(tweet)
-  if tweet.size > 140
+  if tweet.length > 140
       puts word_substituter(tweet)
     else
       return tweet
