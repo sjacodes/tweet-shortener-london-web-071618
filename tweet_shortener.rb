219580a26ecc8ba5e10856994cdafda33a34c70c
Sarah Jacob <smjacob91@gmail.com>
2018-07-08 07:31:20 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+
+def dictionary
+  shortened_words_hash = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  individual_words = tweet.split(" ")
+  
+  
+  # Question 1
+  describe '#word_substituter' do
+    it 'replaces long words with their expected short form' do
+      expect(word_substituter(tweet_one)).to eq(tweet_one_short)
+    end
+  end
